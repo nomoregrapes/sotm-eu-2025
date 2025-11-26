4e8ed74ecf7378510b666cd14e3fd9acfa565db4
James Addison <jay@jp-hosting.net>
2025-11-26 17:54:32 +0000

    Nitpick: sort talks by filenamed start time per room

--- www/assets/js/session_extras.js
+++ www/assets/js/session_extras.js
@@ -9,12 +9,12 @@ window.session_extras = {
         "slides_url": "friday-1130-national-trust.pptx",
         "slides_type": "pptx",
     },
-    "7RREJS": {
-        "slides_url": "friday-1330-ops-sotm-eu-servers-on-fire.pdf",
-        "slides_type": "pdf",
-    },
     "YG9HQC": {
         "slides_url": "friday-1300-andy_allan_our_website.pdf",
+        "slides_type": "pdf",
+    },
+    "7RREJS": {
+        "slides_url": "friday-1330-ops-sotm-eu-servers-on-fire.pdf",
         "slides_type": "pdf",
     },
     "ZSA3CN": {
@@ -34,23 +34,27 @@ window.session_extras = {
         "slides_url": "friday-1300-dave-craig-sotm.pdf",
         "slides_type": "pdf",
     },
-    "DFE3ZQ": {
-        "slides_url": "friday-1530-Jez_Nicholson.pdf",
+    "Z8VSHM": {
+        "slides_url": "friday-1330-Procedural_Buildings.pdf",
         "slides_type": "pdf",
     },
     "YPSJ7Y": {
         "slides_url": "friday-1400-roland-olbricht-funding.pdf",
         "slides_type": "pdf",
     },
+    "DFE3ZQ": {
+        "slides_url": "friday-1530-Jez_Nicholson.pdf",
+        "slides_type": "pdf",
+    },
     "Y8TMQT": {
         "slides_url": "friday-1600-places.pdf",
         "slides_type": "pdf",
     },
-    "Z8VSHM": {
-        "slides_url": "friday-1330-Procedural_Buildings.pdf",
+    //Friday: Way room
+    "UGMYAX": {
+        "slides_url": "friday-1300-State_of_the_Map_Scotland.pdf",
         "slides_type": "pdf",
     },
-    //Friday: Way room
     "LTLLCB": {
         "slides_url": "friday-1330-Mapping_hydropower_schemes.pptx",
         "slides_type": "pptx",
@@ -59,19 +63,7 @@ window.session_extras = {
         "slides_url": "friday-1400-state-of-germany.pdf",
         "slides_type": "pdf",
     },
-    "UGMYAX": {
-        "slides_url": "friday-1300-State_of_the_Map_Scotland.pdf",
-        "slides_type": "pdf",
-    },
     //Saturday: area room
-    "GTDWYA": {
-        "slides_url": "saturday-1100--Using_vector_tiles.odp",
-        "slides_type": "odp",
-    },
-    "SPJQNB": {
-        "slides_url": "saturday-1700-roland_olbricht_overpass.pdf",
-        "slides_type": "pdf",
-    },
     "XCCRKE": {
         "slides_url": "saturday-930-hiking.pdf",
         "slides_type": "pdf",
@@ -80,19 +72,19 @@ window.session_extras = {
         "slides_url": "saturday-1000-survey-me.pdf",
         "slides_type": "pdf",
     },
+    "GTDWYA": {
+        "slides_url": "saturday-1100--Using_vector_tiles.odp",
+        "slides_type": "odp",
+    },
     "VP8YYB": {
         "slides_url": "saturday-1600-spyglass-jochen.pdf",
         "slides_type": "pdf",
     },
+    "SPJQNB": {
+        "slides_url": "saturday-1700-roland_olbricht_overpass.pdf",
+        "slides_type": "pdf",
+    },
     //Saturday: way room
-    "DGZFXR": {
-        "slides_url": "saturday-1700-Getting_notes_working_for_us.pdf",
-        "slides_type": "pdf",
-    },
-    "NH3FYD": {
-        "slides_url": "saturday-1630-schroedingers_data_1.pdf",
-        "slides_type": "pdf",
-    },
     /*
     "THBEEA": {
         //Lightning Talks 4
@@ -104,14 +96,18 @@ window.session_extras = {
         "slides_type": "pdf",
     },
      */
+    "NH3FYD": {
+        "slides_url": "saturday-1630-schroedingers_data_1.pdf",
+        "slides_type": "pdf",
+    },
+    "DGZFXR": {
+        "slides_url": "saturday-1700-Getting_notes_working_for_us.pdf",
+        "slides_type": "pdf",
+    },
     //Saturday: node room
     "73CHAQ": {
         "slides_url": "saturday-0930-01-visualization-analysis-osm-pt-data.pdf",
         "slides_type": "pdf",
-    },
-    "M9KTYY": {
-        "slides_url": "saturday-1600-quality_of_OSM_data_created_by_humanitarian_mappers.pptx",
-        "slides_type": "pptx",
     },
     "VC7W9C": {
         "slides_url": "saturday-1130-MapYourGrid.pdf",
@@ -124,6 +120,10 @@ window.session_extras = {
     },
     "AT3XT9": {
         "slides_url": "saturday-1330-Francis.pptx",
+        "slides_type": "pptx",
+    },
+    "M9KTYY": {
+        "slides_url": "saturday-1600-quality_of_OSM_data_created_by_humanitarian_mappers.pptx",
         "slides_type": "pptx",
     },
     "DDHVTJ": {
